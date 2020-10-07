60a193664a46962940859692a21621f7c0d231e3
Dieqiao Feng <glassices@gmail.com>
2020-10-07 01:03:06 -0400

    before swtching to 8

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -4,13 +4,12 @@
 
 const char* dgemm_desc = "An implementation of GotoBlas";
 
-const int kc = 8;
-const int mc = 1024;
-int aa = 0;
+const int kc = 16;
+const int step_j = 8;
 
 void square_dgemm(const int M, double *A, double *B, double *C)
 {
-    int i, i1, j, j1, k, k1, KC, MC;
+    int i, j, j1, k, k1, KC, MC;
     register __m256d c;
     register __m256d b;
     __m256d a[kc];
@@ -19,86 +18,45 @@ void square_dgemm(const int M, double *A, double *B, double *C)
 
     for (k = 0; k < M; k += kc) {
         KC = M - k < kc ? M - k : kc;
-        for (i = 0; i < M; i += mc) {
-            MC = M - i < mc ? M - i : mc;
-            for (j = 0; j < M; j += 4)
-                for (i1 = i; i1 < i + MC; i1 += 4) {
-                    for (k1 = 0; k1 < kc; k1++)
-                        a[k1] = _mm256_loadu_pd(A + (k + k1) * M + i1);
-                    if (i1 + 4 <= i + MC) {
-                        for (j1 = j; j1 < j + 4 && j1 < M; j1++) {
-                            c = _mm256_loadu_pd(C + j1 * M + i1);
-                            for (k1 = 0, p_B = B + j1 * M + k + k1, p_a = a; k1 < KC; k1++) {
-                                b = _mm256_set1_pd(*(p_B++));
-                                c = _mm256_fmadd_pd(*(p_a++), b, c);
-                                aa++;
-                            }
-                            _mm256_storeu_pd(C + j1 * M + i1, c);
-                        }
+        for (j = 0; j < M; j += step_j) {
+            for (i = 0; i < M / 4 * 4; i += 4) {
+                for (k1 = 0; k1 < kc; k1++)
+                    a[k1] = _mm256_loadu_pd(A + (k + k1) * M + i);
+                for (j1 = j; j1 < j + step_j && j1 < M; j1++) {
+                    c = _mm256_loadu_pd(C + j1 * M + i);
+                    for (k1 = 0, p_B = B + j1 * M + k + k1, p_a = a; k1 < KC; k1++) {
+                        b = _mm256_set1_pd(*(p_B++));
+                        c = _mm256_fmadd_pd(*(p_a++), b, c);
                     }
-                    else if (i1 + 3 == i + MC) {
-                        for (j1 = j; j1 < j + 4 && j1 < M; j1++) {
-                            c = _mm256_loadu_pd(C + j1 * M + i1);
-                            for (k1 = 0, p_B = B + j1 * M + k + k1, p_a = a; k1 < KC; k1++) {
-                                b = _mm256_set1_pd(*(p_B++));
-                                c = _mm256_fmadd_pd(*(p_a++), b, c);
-                                aa++;
-                            }
-                            *(C + j1 * M + i1) = c[0];
-                            *(C + j1 * M + i1 + 1) = c[1];
-                            *(C + j1 * M + i1 + 2) = c[2];
-                        }
+                    _mm256_storeu_pd(C + j1 * M + i, c);
+                }
+            }
+            /*
+            if (M % 4) {
+                i = M / 4 * 4;
+                for (k1 = 0; k1 < kc; k1++)
+                    a[k1] = _mm256_loadu_pd(A + (k + k1) * M + i);
+                for (j1 = j; j1 < j + 4 && j1 < M; j1++) {
+                    c = _mm256_loadu_pd(C + j1 * M + i);
+                    for (k1 = 0, p_B = B + j1 * M + k + k1, p_a = a; k1 < KC; k1++) {
+                        b = _mm256_set1_pd(*(p_B++));
+                        c = _mm256_fmadd_pd(*(p_a++), b, c);
                     }
-                    else if (i1 + 2 == i + MC) {
-                        for (j1 = j; j1 < j + 4 && j1 < M; j1++) {
-                            c = _mm256_loadu_pd(C + j1 * M + i1);
-                            for (k1 = 0, p_B = B + j1 * M + k + k1, p_a = a; k1 < KC; k1++) {
-                                b = _mm256_set1_pd(*(p_B++));
-                                c = _mm256_fmadd_pd(*(p_a++), b, c);
-                                aa++;
-                            }
-                            *(C + j1 * M + i1) = c[0];
-                            *(C + j1 * M + i1 + 1) = c[1];
-                        }
+                    if (i + 3 == M) {
+                        *(C + j1 * M + i) = c[0];
+                        *(C + j1 * M + i + 1) = c[1];
+                        *(C + j1 * M + i + 2) = c[2];
+                    }
+                    else if (i + 2 == M) {
+                        *(C + j1 * M + i) = c[0];
+                        *(C + j1 * M + i + 1) = c[1];
                     }
                     else {
-                        for (j1 = j; j1 < j + 4 && j1 < M; j1++) {
-                            c = _mm256_loadu_pd(C + j1 * M + i1);
-                            for (k1 = 0, p_B = B + j1 * M + k + k1, p_a = a; k1 < KC; k1++) {
-                                b = _mm256_set1_pd(*(p_B++));
-                                c = _mm256_fmadd_pd(*(p_a++), b, c);
-                                aa++;
-                            }
-                            *(C + j1 * M + i1) = c[0];
-                        }
+                        *(C + j1 * M + i) = c[0];
                     }
-
-                    /*
-                    for (k1 = 0; k1 < KC; k1++)
-                        a[k1] = _mm256_loadu_pd(A + (k + k1) * M + i1);
-                    for (j1 = j; j1 < j + 4 && j1 < M; j1++) {
-                        c = _mm256_loadu_pd(C + j1 * M + i1);
-                        for (k1 = 0, p_B = B + j1 * M + k + k1, p_a = a; k1 < KC; k1++) {
-                            b = _mm256_set1_pd(*(p_B++));
-                            c = _mm256_fmadd_pd(*(p_a++), b, c);
-                            aa++;
-                        }
-                        if (i1 + 4 <= i + MC)
-                            _mm256_storeu_pd(C + j1 * M + i1, c);
-                        else if (i1 + 3 == i + MC) {
-                            *(C + j1 * M + i1) = c[0];
-                            *(C + j1 * M + i1 + 1) = c[1];
-                            *(C + j1 * M + i1 + 2) = c[2];
-                        }
-                        else if (i1 + 2 == i + MC) {
-                            *(C + j1 * M + i1) = c[0];
-                            *(C + j1 * M + i1 + 1) = c[1];
-                        }
-                        else
-                            *(C + j1 * M + i1) = c[0];
-                    }
-                    */
                 }
+            }
+            */
         }
     }
 }
@@ -139,9 +97,7 @@ int main()
     free(B);
     free(C);
     printf("%u\n", clock() - start);
-    printf("%d\n", aa);
 
     return 0;
 }
-
 
